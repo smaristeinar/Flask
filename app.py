95ad3635c8a68f21ac60f23630b79fd0da089b56
smari steinar <smarikul@hotmail.com>
2019-08-30 09:12:10 +0000

    new app

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from datetime import datetime
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    return """
+    <h1>Hello heroku</h1>
+    <p>It is currently {time}.</p>
+
+    <img src="http://loremflickr.com/600/400">
+    """
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)
