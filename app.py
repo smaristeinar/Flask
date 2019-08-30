169f16239d4c9286eb444acadc3011bf9dc12a0b
smari steinar <smarikul@hotmail.com>
2019-08-30 09:45:25 +0000

    final

--- app.py
+++ app.py
@@ -7,6 +7,14 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/page2")
+def page2():
+    return render_template("page2.html")
+
+@app.route("/page3")
+def page3():
+    return render_template("page3.html")
+
 
 if __name__ == "__main__":
     app.run()
